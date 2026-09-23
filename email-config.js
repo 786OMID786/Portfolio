0f882335b1f08fcea93626cb06de6cb10679e6f0
Mohammad Omid Noori <m.omid.n786@gmail.com>
2026-09-23 15:35:51 +0430

    Add files via upload

---
+++ email-config.js
@@ -0,0 +1,7 @@
+/* EmailJS browser configuration. The public key is intended for browser use.
+   Never put a private key or email password here. Replace the YOUR_ values. */
+window.EMAILJS_CONFIG = {
+  publicKey: 'CnLCCkSo2L_9wQpyO',
+  serviceId: 'service_4cb0wm7',
+  templateId: 'template_1ogoqxi'
+};
